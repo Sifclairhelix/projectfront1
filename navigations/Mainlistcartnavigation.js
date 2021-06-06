8648ed2736d2d06a532148e4ff64ed6db9b83ffd
Sifclairhelix <62398656+Sifclairhelix@users.noreply.github.com>
2021-06-06 21:35:10 +0100

    Update Mainlistcartnavigation.js

--- navigations/Mainlistcartnavigation.js
+++ navigations/Mainlistcartnavigation.js
@@ -1,21 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-
-/**
-|--------------------------------------------------
-| importing cart : listcart
-| importing listcheckout : checkout this is switched by mistake
-|--------------------------------------------------
-*/
-
 import listcart from "../screens/listcartsection/listcart";
 import listcheckout from "../screens/listcartsection/orderonline/listcheckout";
-
-/**
-|--------------------------------------------------
-| import the listcheckout navigator updated 020421(1)
-|--------------------------------------------------
-*/
 
 import Orderchecknavigation from "../navigations/Orderchecknavigation";
 
@@ -31,13 +17,6 @@ function MyStack() {
 					headerShown: false,
 				}}
 			/>
-			{/* <Stack.Screen
-				name="Checkout"
-				component={Orderchecknavigation}
-				options={{
-					headerShown: false,
-				}}
-			/> */}
 		</Stack.Navigator>
 	);
 }
