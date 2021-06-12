79fd453d2081578eff18a6110e859dde52c483ad
Sifclairhelix <62398656+Sifclairhelix@users.noreply.github.com>
2021-06-12 16:54:12 +0100

    Update Mainfiltercategory.js

--- screens/productsection/Mainfiltercategory.js
+++ screens/productsection/Mainfiltercategory.js
@@ -2,40 +2,18 @@ import React from "react";
 import { View, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
 import { Badge, Text, ListItem } from "native-base";
 
-/**
-|--------------------------------------------------
-| Props will come from the Productmaincontainer
-|--------------------------------------------------
-*/
 
-/**
-|--------------------------------------------------
-|  productCategories={productCategories}
-|  Mainfiltercategory={changeCategory}
-|  productsCategory={productsCategory}
-|  badgeActive={badgeActive}
-|  setBadgeActive={setBadgeActive}
-| 
-|  categories={categories}
-|  categoryFilter={changeCtg}
-|  productsCtg={productsCtg}
-|  active={active}
-|  setActive={setActive}
-|--------------------------------------------------
-*/
 const Mainfiltercategory = (props) => {
 	return (
 		<ScrollView horizontal={true} style={styles.scrollthing}>
 			<ListItem
 				style={
-					styles.styleForList /*{ margin: 0, padding: 0, borderRadius: 0 }*/
+					styles.styleForList 
 				}
 			>
 				<TouchableOpacity
 					key={1}
-					/**
-					 * We will have an onPress event when we pass props props
-					 */
+					
 					onPress={() => {
 						props.Mainfiltercategory("all"), props.setBadgeActive(-1);
 					}}
@@ -59,35 +37,20 @@ const Mainfiltercategory = (props) => {
 				</TouchableOpacity>
 				{props.productCategories.map((item) => (
 					<TouchableOpacity
-						/**
-						|--------------------------------------------------
-						| This orginally is item._id.$oid but when getting data it is item._id
-						|--------------------------------------------------
-						*/
+						
 						key={item._id}
-						/**
-						 * We will have an onPress event when we pass props props
-						 */
+						
 						onPress={() => {
-							/**
-							|--------------------------------------------------
-							| props.Mainfiltercategory(item._id.$oid), this will change into this i guess props.Mainfiltercategory(item._id) not now but later
-							| Here we have a function where we change the  props.Mainfiltercategory(item._id.$oid), to props.Mainfiltercategory(item._id),
-							|--------------------------------------------------
-							*/
+							
 							props.Mainfiltercategory(item._id),
 								props.setBadgeActive(props.productCategories.indexOf(item));
 						}}
 					>
 						<Badge
-							/**
-                    |--------------------------------------------------
-                    | this badge is how the category buttons look like
-                    |--------------------------------------------------
-                    */
+							
 							style={[
 								styles.badgeCenterStruc,
-								/*{ margin: 5 },*/
+								
 								props.badgeActive == props.productCategories.indexOf(item)
 									? styles.badgeActive
 									: styles.badgeInactive,
@@ -102,11 +65,7 @@ const Mainfiltercategory = (props) => {
 	);
 };
 
-/**
-|--------------------------------------------------
-| Stylesheet goes over here change it later
-|--------------------------------------------------
-*/
+
 
 const styles = StyleSheet.create({
 	badgeCenterStruc: {
